c0380ecd5fde8b001ab48db1e6c83262ee1566e3
Cristian Andrescu <Cristian Andrescu>
2016-10-25 13:20:17 +0300

    trei

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Site nou </div>
+                <div class="title">Site nou de istorie </div>
             </div>
         </div>
     </body>
